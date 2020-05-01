fe371ecfacfd037aaca7bd7dd3d83af3a45faef8
Reuben <gathiireuben@gmail.com>
2020-05-01 18:27:48 +0300

    updates Squad class

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,34 +1,33 @@
 package models;
 
-
 import java.util.ArrayList;
 
 public class Squad {
-    private String squadName;
+    private String name;
     private int maxNum;
     private static ArrayList<Squad> instances = new ArrayList<>();
 
     private String cause;
 
-    public Squad(String squadName, int maxNum, String cause){
-        this.squadName= squadName;
+    public Squad(String name, int maxNum, String cause){
+        this.name= name;
         this.maxNum= maxNum;
         this.cause = cause;
+        instances.add(this);
         
     }
 
-    public static ArrayList<Squad> getAll() {
-        return instances;
-    }
-
     public String getName(){
-        return squadName;
+        return name;
     }
 
     public int getMaxNum(){return maxNum;}
 
     public String getCause(){return cause;}
 
+    public static ArrayList<Squad> getAll() {
+        return instances;
+    }
 
 
 }
